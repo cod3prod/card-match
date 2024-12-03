1ea5e56b427ec111fcab94bea86cffe2d2c2e0d9
cod3prod <cod3prod@gmail.com>
2024-12-03 16:01:25 +0900

    🐛 Fix displayname of DescriptionItem

--- src/components/description-item.tsx
+++ src/components/description-item.tsx
@@ -3,7 +3,11 @@
 import React from "react";
 
 const DescriptionItem = React.memo(({ value }: { value: string }) => {
-  return <li className="text-info mb-1 ">{`You matched the pair of ${value}.`}</li>;
+  return (
+    <li className="text-info mb-1 ">{`You matched the pair of ${value}.`}</li>
+  );
 });
+
+DescriptionItem.displayName = "DescriptionItem";
 
 export default DescriptionItem;
